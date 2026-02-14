87a162aee3dd9fce56ffda84aee2b96f25dfca1c
AlexMcFly <95719167+MaddestOfLads@users.noreply.github.com>
2026-02-14 21:22:34 +0100

    Add check if island is within range

--- Script/island.gd
+++ Script/island.gd
@@ -3,7 +3,7 @@ class_name Island extends Area2D
 
 @export var id: int = -1
 @export var radius_circle : RadiusCircle = null
-@export var radius : int = 5
+@export var base_radius : int = 5
 
 const RADIUS_MULTIPLIER : int = 200
 
@@ -17,7 +17,7 @@ signal island_no_longer_hovered(island)
 var connected_bridges : Array[Bridge] = []
 
 func _ready() -> void:
-	radius_circle.radius = radius * RADIUS_MULTIPLIER
+	radius_circle.radius = base_radius * RADIUS_MULTIPLIER
 	mouse_entered.connect(on_mouse_entered)
 	mouse_exited.connect(on_mouse_exited)
 	modulate = _default_modulate
@@ -46,3 +46,10 @@ func is_other_island_already_connected(other : Island) -> bool:
 		if (connected_bridges[i].island_1 == other or self.connected_bridges[i].island_2 == other):
 			return true
 	return false
+
+func effective_radius() -> int:
+	return (base_radius + connected_bridges.size()) * RADIUS_MULTIPLIER
+
+func is_other_island_within_range(other : Island) -> bool:
+	var dist = position.distance_to(other.position)
+	return (dist <= effective_radius() and dist <= other.effective_radius())
