f237941ee16f4dd17d475a2d147fcfb04e573779
AlexMcFly <95719167+MaddestOfLads@users.noreply.github.com>
2026-02-14 14:44:13 +0100

    Implement UI

---
+++ Script/ui.gd
@@ -0,0 +1,11 @@
+class_name Ui extends Control
+
+@export var race_name_label : RichTextLabel = null
+@export var race_description_label : RichTextLabel = null
+@export var race_image_texture_rect : TextureRect = null
+
+# TODO: add method for setting race name, description, and portrait at once, which will also show this UI if it is hidden
+# TODO: add method for clearing race name, description, and portrait, which will also hide this UI
+
+func _ready() -> void:
+	pass
